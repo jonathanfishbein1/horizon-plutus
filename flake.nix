e8ddfa92180fab57e1430037dd8c3b464f646db9
Daniel Firth <dan.firth@homotopic.tech>
2022-10-25 12:44:28 +0100

    horizon-gen-nix: switch to use version in horizon-platform

--- flake.nix
+++ flake.nix
@@ -4,9 +4,8 @@
     lint-utils.url = "git+https://gitlab.homotopic.tech/nix/lint-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-libR.url = "github:nixos/nixpkgs/602748c14b82a2e17078713686fe1df2824fa502";
-    horizon-gen-nix.url = "git+https://gitlab.homotopic.tech/horizon/horizon-gen-nix";
   };
-  outputs = inputs@{ self, nixpkgs, nixpkgs-libR, horizon-gen-nix, horizon-platform, flake-utils, lint-utils, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-libR, horizon-platform, flake-utils, lint-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs-libR = import nixpkgs-libR { inherit system; };
@@ -31,7 +30,7 @@
         apps = {
           horizon-gen-nix = {
             type = "app";
-            program = "${horizon-gen-nix.outputs.packages.x86_64-linux.default}/bin/horizon-gen-nix";
+            program = "${hp.horizon-gen-nix}/bin/horizon-gen-nix";
           };
         };
         checks = {

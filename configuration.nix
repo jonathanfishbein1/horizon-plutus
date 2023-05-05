e90df00376ed4f5725023b9a71d60763a69feaac
Daniel Firth <dan.firth@homotopic.tech>
2023-05-05 15:56:00 +0000

    configuration.nix: cleanup

--- configuration.nix
+++ configuration.nix
@@ -14,69 +14,13 @@ final: prev: {
 
   cardano-prelude = enableCabalFlag prev.cardano-prelude "development";
 
-  ghcide = null;
-
-  ghcide-bench = null;
-
-  ghcide-test-utils = null;
-
-  haskell-language-server = null;
-
-  hiedb = null;
-
-  hls-alternate-number-format-plugin = null;
-
-  hls-cabal-fmt-plugin = null;
-
-  hls-cabal-plugin = null;
-
-  hls-call-hierarchy-plugin = null;
-
-  hls-change-type-signature-plugin = null;
-
-  hls-class-plugin = null;
-
-  hls-code-range-plugin = null;
-
-  hls-explicit-fixity-plugin = null;
-
-  hls-explicit-imports-plugin = null;
-
-  hls-explicit-record-fields-plugin = null;
-
-  hls-fourmolu-plugin = null;
-
-  hls-gadt-plugin = null;
-
-  hls-hlint-plugin = null;
-
-  hls-module-name-plugin = null;
-
-  hls-pragmas-plugin = null;
-
-  hls-qualify-imported-names-plugin = null;
-
-  hls-refactor-plugin = null;
-
-  hls-refine-imports-plugin = null;
-
-  hls-retrie-plugin = null;
-
-  hls-splice-plugin = null;
-
-  hls-test-utils = null;
-
   plutus-core = pkgs.haskell.lib.addSetupDepend prev.plutus-core pkgs.haskell.packages.ghc925.Cabal_3_8_1_0;
 
   plutus-tx = pkgs.haskell.lib.addSetupDepend prev.plutus-tx pkgs.haskell.packages.ghc925.Cabal_3_8_1_0;
 
   plutus-tx-plugin = pkgs.haskell.lib.addSetupDepend prev.plutus-tx-plugin pkgs.haskell.packages.ghc925.Cabal_3_8_1_0;
 
-#  plutus-core = setBuildTarget (dontBenchmark prev.plutus-core) "plc";
-
   plutus-ledger-api = dontBenchmark prev.plutus-ledger-api;
-
-#  plutus-tx = dontBenchmark prev.plutus-tx;
 
   secp256k1 = pkgs.secp256k1;
 
